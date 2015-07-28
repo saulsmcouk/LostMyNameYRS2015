974ed88e05a8effd4b4d04fc470e4c75258571a1
Malachy Hall Thorpe <me@malachy.org.uk>
2015-07-28 09:42:28 +0100

    More cleanup and passing through req

--- python-backend/server.py
+++ python-backend/server.py
@@ -4,6 +4,7 @@ import sys_modules.database as db
 
 import api_modules.offer
 import api_modules.message
+import api_modules.login
 
 app = Flask(__name__)
 
@@ -23,27 +24,27 @@ def offer_add():
 @app.route('/offer/get/id')
 def offer_get_id():
 
-    return api_modules.offer.offer_get_id()
+    return api_modules.offer.offer_get_id(request)
 
 @app.route('/offer/get/near_me/<coords>')
 def offer_near_me(coords):
 
-    return api_modules.offer.offer_get_near_me()
+    return api_modules.offer.offer_get_near_me(request, coords)
 
 @app.route('/message/send', methods=['POST'])
 def message_send():
 
-    return api_modules.message.message_send()
+    return api_modules.message.message_send(request)
 
 @app.route('/message/inbox')
 def message_inbox():
 
-    return api_modules.message.inbox()
+    return api_modules.message.inbox(request)
 
 @app.route('/message/read', methods=['POST'])
 def message_read():
 
-    return api_modules.message.read()
+    return api_modules.message.read(request)
 
 if __name__ == '__main__':
     app.run(host='192.168.56.102',port=5000,debug=True)
