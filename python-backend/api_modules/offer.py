ccafbe8985e2bf23247a20c3b128d678687ad6b5
Laurie Hallows <lauriehallows@Lauries-MacBook-Pro.local>
2015-07-28 10:44:36 +0100

    completed offer add function

--- python-backend/api_modules/offer.py
+++ python-backend/api_modules/offer.py
@@ -1,5 +1,18 @@
+import sys_modules.database as db
+
 def offer_add(request):
-    print request.form
+    coords = request.form.getlist('location')[0].split(',')
+    coords_array=[coords[0],coords[1]]
+    offer= {
+        'title': request.form.getlist('title')[0],
+        'description': request.form.getlist('description')[0],
+        'username': request.form.getlist('username')[0],
+        'ttl': request.form.getlist('ttl')[0],
+        'location': coords_array
+    }
+    
+    db.db_insert('offers',offer)
+    
     return "offer add"
 
 def offer_get_id(request):
