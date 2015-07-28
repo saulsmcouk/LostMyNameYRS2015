c77d55e6a90ea6c65841e698fc5528c26d9d8d7a
saulsmcouk <ssm8@hotmail.co.uk>
2015-07-28 10:47:08 +0100

    Message set up

--- python-backend/api_modules/message.py
+++ python-backend/api_modules/message.py
@@ -1,3 +1,10 @@
+import sys_modules.database as db
+
 def message_send(request):
-    a = str(request.form)
-    return a
+    message_stuff = request.args#.getlist('l')[0]
+    message = {
+                            'to':  message_stuff.getlist('to'),
+                            'from': message_stuff.getlist('from'),
+                            'content': message_stuff.getlist('content')}
+    db.db_insert("messages",message)
+    return str(message)
